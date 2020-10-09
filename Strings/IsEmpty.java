7cd5b86d12f207f05a15ed5b7db3ffd70fd90ff7
Ravi Kumar <ravi93448@yahoo.com>
2020-10-09 18:11:49 +0530

    Strings

--- Strings/IsEmpty.java
+++ Strings/IsEmpty.java
@@ -1,27 +1,18 @@
 // Check String isEmpty ?
 
-import java.io.UnsupportedEncodingException;
+
 import java.util.*;
 
-class EncodeNuberToChar{
+class IsEmpty{
 public static void main(String args[]){
 
     Scanner sc=new Scanner(System.in);
 
-    //System.out.println("Enter number That U want to convert in Character = ");
-    //String str1=sc.nextLine();
+    String str="abcd";
     
-    //byte []a=str1.getBytes();
-    byte[]a={65,66};
-    System.out.println(a);
-    String str2="";
-    try{
-        str2=new String(a,"US-ASCII");
-    }
-    catch(UnsupportedEncodingException e){
-        e.printStackTrace();
-    }
-    System.out.println(str2);
+    System.out.println(str.isEmpty());
+    str="";
+    System.out.println(str.isEmpty());
 
 
 
